b70333bb13dc668dcf07ca433d58c7b016af389c
Matej Drzik <matej.drzik@quanto.sk>
2016-03-13 11:01:21 +0100

    0.0.25 eventMarker fix

--- nodes/proximiioEventMarker.js
+++ nodes/proximiioEventMarker.js
@@ -11,17 +11,19 @@ module.exports = function(RED) {
 
         this.inputRef = new Firebase(RED.settings.proximiio.organization.eventBusRef + '/proximity');
         this.on('input', function(data) {
-            var inputEventId = data._proximi_id;
-            // mark processed
+            if (typeof data._proximi_id != "undefined") {
+              var inputEventId = data._proximi_id;
+              // mark processed
 
-            node.inputRef.child(inputEventId).update({ processed: true }, function(error) {
+              node.inputRef.child(inputEventId).update({ processed: true }, function(error) {
                 if (error) {
                     node.status({fill:"red",shape:"ring",text:"error occured"});
                 } else {
                     node.totalCount++;
                     node.status({fill:"yellow",shape:"ring",text:"waiting (" + node.totalCount  + ")"});
                 }
-            });
+              });
+            }
         });
 
         this.on('close', function(done) {
--- server.js
+++ server.js
@@ -171,7 +171,7 @@ var server = http.createServer(app);
 
 program._name = 'proximiio';
 program
-  .version('0.0.24');
+  .version('0.0.25');
 
 program
   .command('start')
