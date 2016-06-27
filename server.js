9ecb910234ff5a1e3805db4f70899b5559fc0816
Tattek <matej.drlicka@quanto.sk>
2016-06-27 11:12:52 +0200

    estimote import fixed
    tangram plugin update

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.post('/instance', postInstance);
 var server = http.createServer(app);
 
 program._name = 'proximiio';
-program.version('0.0.57');
+program.version('0.0.58');
 
 program.command('start').description('initialize proximi.io portal').action(function() {
   console.log('initializing portal...');
